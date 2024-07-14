f5493c17223226c317420bbcbffded1e1ff38728
wolfsilver <wolfsilver@users.noreply.github.com>
2024-07-14 00:46:03 +0000

    feat: add ACCESS_TOKEN in .env.example

--- config.go
+++ config.go
@@ -8,8 +8,9 @@ import (
 )
 
 type Config struct {
-	ServerAddr string `env:"SERVER_ADDR,required"`
-	BotToken   string `env:"BOT_TOKEN,required"`
+	ServerAddr  string `env:"SERVER_ADDR,required"`
+	BotToken    string `env:"BOT_TOKEN,required"`
+	AccessToken string `env:"ACCESS_TOKEN"`
 }
 
 func getConfigFromEnv() (*Config, error) {
