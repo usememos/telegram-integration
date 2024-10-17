13d5f045552a719282a562b0b0d5e26395a08f55
cleardevice <cleardevice@gmail.com>
2024-10-17 15:35:44 +0000

    feat: add simple message.Entities handle

--- memogram.go
+++ memogram.go
@@ -7,8 +7,10 @@ import (
 	"log/slog"
 	"net/http"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"time"
+	"unicode/utf16"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
@@ -138,35 +140,39 @@ func (s *Service) handleMemoCreation(ctx context.Context, m *models.Update, cont
 }
 
 func (s *Service) handler(ctx context.Context, b *bot.Bot, m *models.Update) {
-	if strings.HasPrefix(m.Message.Text, "/start ") {
+	message := m.Message
+	if strings.HasPrefix(message.Text, "/start ") {
 		s.startHandler(ctx, b, m)
 		return
-	} else if strings.HasPrefix(m.Message.Text, "/search ") {
+	} else if strings.HasPrefix(message.Text, "/search ") {
 		s.searchHandler(ctx, b, m)
 		return
 	}
 
-	userID := m.Message.From.ID
+	userID := message.From.ID
 	if _, ok := s.store.GetUserAccessToken(userID); !ok {
 		b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: m.Message.Chat.ID,
+			ChatID: message.Chat.ID,
 			Text:   "Please start the bot with /start <access_token>",
 		})
 		return
 	}
 
-	message := m.Message
-	// TODO: handle message.Entities to get markdown text.
 	content := message.Text
+	contentEntities := message.Entities
 	if message.Caption != "" {
 		content = message.Caption
+		contentEntities = message.CaptionEntities
+	}
+	if len(contentEntities) > 0 {
+		content = formatContent(content, contentEntities)
 	}
 
 	// Add "forwarded from: originName" if message was forwarded
-	if m.Message.ForwardOrigin != nil {
+	if message.ForwardOrigin != nil {
 		var originName, originUsername string
 		// Determine the type of origin
-		switch origin := m.Message.ForwardOrigin; {
+		switch origin := message.ForwardOrigin; {
 		case origin.MessageOriginUser != nil: // User
 			user := origin.MessageOriginUser.SenderUser
 			if user.LastName != "" {
@@ -202,7 +208,7 @@ func (s *Service) handler(ctx context.Context, b *bot.Bot, m *models.Update) {
 	hasResource := message.Document != nil || len(message.Photo) > 0 || message.Voice != nil || message.Video != nil
 	if content == "" && !hasResource {
 		b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: m.Message.Chat.ID,
+			ChatID: message.Chat.ID,
 			Text:   "Please input memo content",
 		})
 		return
@@ -215,7 +221,7 @@ func (s *Service) handler(ctx context.Context, b *bot.Bot, m *models.Update) {
 	memo, err := s.handleMemoCreation(ctx, m, content)
 	if err != nil {
 		b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: m.Message.Chat.ID,
+			ChatID: message.Chat.ID,
 			Text:   "Failed to create memo",
 		})
 		return
@@ -236,7 +242,7 @@ func (s *Service) handler(ctx context.Context, b *bot.Bot, m *models.Update) {
 	}
 
 	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID:              m.Message.Chat.ID,
+		ChatID:              message.Chat.ID,
 		Text:                fmt.Sprintf("Content saved as %s with [%s](%s/m/%s)", v1pb.Visibility_name[int32(memo.Visibility)], memo.Name, s.workspaceProfile.InstanceUrl, memo.Uid),
 		ParseMode:           models.ParseModeMarkdown,
 		DisableNotification: true,
@@ -472,4 +478,49 @@ func (s *Service) sendError(b *bot.Bot, chatID int64, err error) {
 		ChatID: chatID,
 		Text:   fmt.Sprintf("Error: %s", err.Error()),
 	})
+}
+
+func formatContent(content string, contentEntities []models.MessageEntity) string {
+	contentRunes := utf16.Encode([]rune(content))
+
+	var sb strings.Builder
+	var prevEntity = models.MessageEntity{}
+	var entityContent string
+	re := regexp.MustCompile("^(\\s*)(.*)(\\s*)$")
+
+	for _, entity := range contentEntities {
+		switch entity.Type {
+		case models.MessageEntityTypeURL:
+		case models.MessageEntityTypeTextLink:
+		case models.MessageEntityTypeBold:
+		case models.MessageEntityTypeItalic:
+		default:
+			continue
+		}
+
+		if entity.Offset >= prevEntity.Offset+prevEntity.Length {
+			sb.WriteString(entityContent)
+			sb.WriteString(string(utf16.Decode(contentRunes[prevEntity.Offset+prevEntity.Length : entity.Offset])))
+			entityContent = string(utf16.Decode(contentRunes[entity.Offset : entity.Offset+entity.Length]))
+			prevEntity = entity
+			if strings.TrimSpace(entityContent) == "" {
+				continue
+			}
+		}
+
+		matches := re.FindStringSubmatch(entityContent)
+		switch entity.Type {
+		case models.MessageEntityTypeURL:
+			entityContent = fmt.Sprintf("%s[%s](%s)%s", matches[1], matches[2], matches[2], matches[3])
+		case models.MessageEntityTypeTextLink:
+			entityContent = fmt.Sprintf("%s[%s](%s)%s", matches[1], matches[2], entity.URL, matches[3])
+		case models.MessageEntityTypeBold:
+			entityContent = fmt.Sprintf("%s**%s**%s", matches[1], matches[2], matches[3])
+		case models.MessageEntityTypeItalic:
+			entityContent = fmt.Sprintf("%s*%s*%s", matches[1], matches[2], matches[3])
+		}
+	}
+	sb.WriteString(entityContent)
+	sb.WriteString(string(utf16.Decode(contentRunes[prevEntity.Offset+prevEntity.Length:])))
+	return sb.String()
 }
